bf989ea13a091c0c24f138d57eb31a8907b5f59f
Fayyaz Alam <fayazalam.98@gmail.com>
2021-07-02 10:44:54 -0700

    data in objects added

---
+++ app.js
@@ -0,0 +1,64 @@
+var phones={
+  oppo:{
+      oppof5:{
+        name:"oppof5",
+        color:"blue",
+        price:"20k",
+        url:"oppo/f5.jpg"
+      } , 
+      oppof9:{
+        name:"oppof9",
+        color:"white",
+        price:"25k",
+        url:"oppo/f9.jpg"
+      }  ,
+      oppof11:{
+        name:"oppof11",
+        color:"black",
+        price:"30k",
+        url:"oppo/f11.jpg"
+      }  
+  },
+  iphone:{
+      iphone5:{
+          name:"iphone5",
+          color:"black",
+          price:"10k",
+          url:"iphone/i5.jpg"
+        },
+      iphonex:{
+          name:"iphonex",
+          color:"white",
+          price:"100k",
+          url:"iphone/ix.jpg"
+        },
+      iphone12:{
+          name:"iphone12",
+          color:"black",
+          price:"10k",
+          url:"iphone/i12.jpg"
+        },
+  },
+  samsung:{
+      samsungs4:{
+          name:"samsungs4",
+          color:"grey",
+          price:"12k",
+          url:"samsung/s4.jpg"
+        },    
+      samsungs8:{
+          name:"samsungs8",
+          color:"black",
+          price:"60k",
+          url:"samsung/s4.jpg"
+        },    
+      samsungs10:{
+          name:"samsungs10",
+          color:"white",
+          price:"100k",
+          url:"samsung/s4.jpg"
+        },    
+  }
+}
+
+
